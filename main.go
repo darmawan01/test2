1ef6284aeae4734d6bce197b6d427bfd41c81983
darmawan01 <darmawan.zulkifli@gmail.com>
2019-02-28 19:01:46 +0800

    done

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"sort"
+	"strings"
 )
+
+// Word models
+type Word struct {
+	Text string `json:"text"`
+}
 
 func logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -13,8 +20,22 @@ func logging(f http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func sorting(s string) string {
+	text := strings.ToLower(s)
+	newText := strings.Split(text, "")
+	sort.Strings(newText)
+
+	return strings.Join(newText, "")
+}
+
 func welcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my website!")
+	var word Word
+	json.NewDecoder(r.Body).Decode(&word)
+
+	resolve := sorting(word.Text)
+
+	word.Text = resolve
+	json.NewEncoder(w).Encode(word)
 }
 
 func main() {
